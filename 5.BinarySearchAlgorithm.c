d2e1ef4ed326e92d43cb3876fad6d6e936ccb7e7
Aadarsha Acharya <34684305+becomingadarsha@users.noreply.github.com>
2018-08-13 06:44:51 +0545

    Update 5.BinarySearchAlgorithm.c

--- 5.BinarySearchAlgorithm.c
+++ 5.BinarySearchAlgorithm.c
@@ -1,48 +1,44 @@
 /*
 A Recursive Binary Search Algorithm.
 */
-#include<stdio.h>
+#include <stdio.h>
 
-int binarySearch(int x[],int element,int start,int end);
-int main()
+int binarysearch(int a[], int low, int high, int x)
 {
-	int x[20],n,i,index,start=0,end,element;
-	printf("Enter number of  elements: ");
-	scanf("%d",&n);
-	end = n;
-	printf("Enter array elements: ");
-	for(i=0;i<n;i++){
-		scanf("%d",&x[i]);
-	}
-	printf("Enter the element to search: ");
-	scanf("%d",&element);
-	index = binarySearch(x,element,start,end-1);
-	if(index == -1)
-		printf("Element Not Found.\n");
-	else
-		printf("Element found at index : %d\n",index);
+   int mid = (low + high) / 2;
+   if (low > high)
+	   return -1;
+   if (a[mid] == x) 
+	   return mid;
 
-
-	return 0;
+   if (a[mid] < x)
+     return binarysearch(a, mid + 1, high, x);
+   else
+     return binarysearch(a, low, mid-1, x);
 }
 
-int binarySearch(int x[],int element,int start,int end)
+int main(void)
 {
-	int mid,noOfElements,i;
-	mid = (int)(start+end)/2;
-	if(start > end)
-		return -1;
-	if(x[mid] == element)
-		return mid;
-	else if(x[mid] < element)
-	{
-		start = mid+1;
-		binarySearch(x,element,start,end);
-	}
-	else{
-		start = 0;
-		end = mid-1;
-		binarySearch(x,element,start,end);
-	}
+  int a[100];
+  int len, pos, search_item;
 
+  printf("Enter the length of the array\n");
+  scanf("%d", &len);
+
+  printf("Enter the array elements\n");
+  for (int i=0; i<len; i++)
+    scanf("%d", &a[i]);
+
+  printf("Enter the element to search\n");
+  scanf("%d", &search_item);
+
+  pos = binarysearch(a,0,len-1,search_item);
+
+  if (pos < 0 )
+    printf("Cannot find the element %d in the array.\n", search_item);
+
+  else
+    printf("The position of %d in the array is %d.\n", search_item, pos+1);
+
+  return 0;
 }
