2ab91d0d11938c4f6bc1facba52a8f38ef4b3dc3
Aadarsha Acharya <34684305+becomingadarsha@users.noreply.github.com>
2018-08-13 06:40:28 +0545

    Update 5.BinarySearchAlgorithm.c

--- 5.BinarySearchAlgorithm.c
+++ 5.BinarySearchAlgorithm.c
@@ -4,7 +4,8 @@ A Recursive Binary Search Algorithm.
 #include<stdio.h>
 
 int binarySearch(int x[],int element,int start,int end);
-int main(){
+int main()
+{
 	int x[20],n,i,index,start=0,end,element;
 	printf("Enter number of  elements: ");
 	scanf("%d",&n);
@@ -25,14 +26,16 @@ int main(){
 	return 0;
 }
 
-int binarySearch(int x[],int element,int start,int end){
+int binarySearch(int x[],int element,int start,int end)
+{
 	int mid,noOfElements,i;
 	mid = (int)(start+end)/2;
 	if(start > end)
 		return -1;
 	if(x[mid] == element)
 		return mid;
-	else if(x[mid] < element){
+	else if(x[mid] < element)
+	{
 		start = mid+1;
 		binarySearch(x,element,start,end);
 	}
