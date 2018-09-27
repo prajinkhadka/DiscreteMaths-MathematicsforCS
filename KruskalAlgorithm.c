9af6932d93e77a94210fbfbc4172eaa632eeb1b6
Aadarsha Acharya <34684305+becomingadarsha@users.noreply.github.com>
2018-09-27 10:18:28 +0545

    Update KruskalAlgorithm.c

--- KruskalAlgorithm.c
+++ KruskalAlgorithm.c
@@ -6,7 +6,7 @@ int i,j,k,a,b,u,v,n,ne=1;
 int min,mincost=0,cost[9][9],parent[9];
 int main()
 {
-    printf("\Implementation of Kruskal's Algorithm: ");
+    printf("\nImplementation of Kruskal's Algorithm: ");
     printf("\nEnter no. of vertices: ");
     scanf("%d",&n);
     printf("\nEnter the cost adjacency matrix: \n");
