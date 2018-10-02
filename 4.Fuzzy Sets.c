c8c05a0951612ce9813517205c1169d09821065c
Aadarsha Acharya <34684305+becomingadarsha@users.noreply.github.com>
2018-10-02 19:16:11 +0545

    Update 4.Fuzzy Sets.c

--- 4.Fuzzy Sets.c
+++ 4.Fuzzy Sets.c
@@ -19,117 +19,112 @@ float a[5],b[5],c[5];
 char name[5][15];
 void unio()
 {
-	for(i=0;i<5;i++)
-	{
-		if(a[i]>=b[i])
-		c[i]=a[i];
-		else 
-		c[i]=b[i];
-	}
-	printf("union\n");
-	printf("{  ");
-	for(i=0;i<5;i++)
-	{
-		printf("%.2f / %s ,",c[i],name[i]);
-	}
-	printf("}");
+ for(i=0;i<5;i++)
+ {
+ if(a[i]>=b[i])
+ c[i]=a[i];
+ else 
+ c[i]=b[i];
+ }
+ printf("union\n");
+ printf("{ ");
+ for(i=0;i<5;i++)
+ {
+ printf("%.2f / %s ,",c[i],name[i]);
+ }
+ printf("}");
 }
 void intersection()
 {
-	for(i=0;i<5;i++)
-	{
-		if(a[i]<=b[i])
-		c[i]=a[i];
-		else
-		c[i]=b[i];
-	}
-	printf("\nintersection\n");
-	printf("{");
-	for(i=0;i<5;i++)
-	{
-		printf("%.2f / %s , ",c[i],name[i]);
-	}
-	printf("}");
+ for(i=0;i<5;i++)
+ {
+ if(a[i]<=b[i])
+ c[i]=a[i];
+ else
+ c[i]=b[i];
+ }
+ printf("\nintersection\n");
+ printf("{");
+ for(i=0;i<5;i++)
+ {
+ printf("%.2f / %s , ",c[i],name[i]);
+ }
+ printf("}");
 }
 void complement()
 {
-	printf("\ncomplement\n");
-	printf("complement of A");
-	printf("{");
-	for(i=0;i<5;i++)
-	{
-	printf("%.2f / %s ,",1.00-a[i],name[i]);
-	
-	}
-	printf("}");
-	printf("\ncomplement of B");
-	printf("{ ");
-	for(i=0;i<5;i++)
-	{
-	printf("%.2f / %s ,",1.00-b[i],name[i]);
-	
-	}
-	printf("}");
-	
-}
-
-	
+ printf("\ncomplement\n");
+ printf("complement of A");
+ printf("{");
+ for(i=0;i<5;i++)
+ {
+ printf("%.2f / %s ,",1.00-a[i],name[i]);
+ }
+ printf("}");
+ printf("\ncomplement of B");
+ printf("{ ");
+ for(i=0;i<5;i++)
+ {
+ printf("%.2f / %s ,",1.00-b[i],name[i]);
+ 
+ }
+ printf("}");
+ }
 int main()
 {
-	for(i=0;i<5;i++)
-	{
-		printf("Name=\t");
-		scanf("%s",&name[i]);
-		printf("Age=\t");
-		scanf("%d",&age[i]);
-	}
-	printf("{ ");
-	for(i=0;i<5;i++)
-	{
-		if(age[i]<=20)
-		{
-			a[i]=1;
-			printf("%.2f / %s ",a[i],name[i]);
-		}
-		else if((age[i]>20)&&(age[i]<=30))
-		{
-			a[i]=(30-age[i])/10.00;
-		  printf("%.2f / %s",a[i],name[i]);	
-		}
-		else if(age[i]>30)
-		{
-			a[i]=0;
-			printf("%.2f / %s",a[i],name[i]);
-		}
-		printf(", ");
-	}
-	printf("}");
-	printf("\n");
-		printf("{ ");
-	for(i=0;i<5;i++)
-	{
-		if(age[i]<=15)
-		{
-			b[i]=1;
-			printf("%.2f / %s ",b[i],name[i]);
-		}
-		else if((age[i]>15)&&(age[i]<=35))
-		{
-			b[i]=(30-age[i])/20.00;
-		  printf("%.2f / %s",b[i],name[i]);	
-		}
-		else if(age[i]>35)
-		{
-			b[i]=0.00;
-			printf("%.f / %s",b[i],name[i]);
-		}
-		printf(", ");
-	}
-	printf("}");
-	printf("\n");
-	unio();
-	intersection();
-	complement();
-	return 0;
-	
+ for(i=0;i<5;i++)
+ {
+ printf("Name=\t");
+ scanf("%s",&name[i]);
+ printf("Age=\t");
+ scanf("%d",&age[i]);
+ }
+ printf("{ ");
+ for(i=0;i<5;i++)
+ {
+ if(age[i]<=20)
+ {
+ a[i]=1;
+ printf("%.2f / %s ",a[i],name[i]);
+ }
+ else if((age[i]>20)&&(age[i]<=30))
+ {
+ a[i]=(30-age[i])/10.00;
+ printf("%.2f / %s",a[i],name[i]); 
+ }
+ else if(age[i]>30)
+ {
+ a[i]=0;
+ printf("%.2f / %s",a[i],name[i]);
+ }
+ printf(", ");
+ }
+ printf("}");
+ printf("\n");
+ printf("{ ");
+ for(i=0;i<5;i++)
+ {
+ if(age[i]<=15)
+ {
+ b[i]=1;
+ printf("%.2f / %s ",b[i],name[i]);
+ }
+ else if((age[i]>15)&&(age[i]<=35))
+ {
+ b[i]=(30-age[i])/20.00;
+ printf("%.2f / %s",b[i],name[i]); 
+ }
+ else if(age[i]>35)
+ {
+ b[i]=0.00;
+ printf("%.f / %s",b[i],name[i]);
+ }
+ printf(", ");
+ }
+ printf("}");
+ printf("\n");
+ unio();
+ intersection();
+ complement();
+ return 0;
 }
