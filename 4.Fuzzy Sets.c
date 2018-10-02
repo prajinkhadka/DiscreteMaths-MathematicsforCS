a86a658684088e941a1afe089ec48609d1ae9c7b
Aadarsha Acharya <34684305+becomingadarsha@users.noreply.github.com>
2018-10-02 19:22:48 +0545

    Update 4.Fuzzy Sets.c

--- 4.Fuzzy Sets.c
+++ 4.Fuzzy Sets.c
@@ -12,7 +12,6 @@ Degree of membership = 0 				if age>35
 Perform set operations according to rules of fuzzy sets, on these two sets.
 
 */
-
 #include<stdio.h>
 int i,age[5];
 float a[5],b[5],c[5];
@@ -21,16 +20,16 @@ void unio()
 {
  for(i=0;i<5;i++)
  {
- if(a[i]>=b[i])
- c[i]=a[i];
- else 
- c[i]=b[i];
+  if(a[i]>=b[i])
+  c[i]=a[i];
+  else
+  c[i]=b[i];
  }
  printf("union\n");
- printf("{ ");
+ printf("{  ");
  for(i=0;i<5;i++)
  {
- printf("%.2f / %s ,",c[i],name[i]);
+  printf("%.2f / %s ,",c[i],name[i]);
  }
  printf("}");
 }
@@ -38,16 +37,16 @@ void intersection()
 {
  for(i=0;i<5;i++)
  {
- if(a[i]<=b[i])
- c[i]=a[i];
- else
- c[i]=b[i];
+  if(a[i]<=b[i])
+  c[i]=a[i];
+  else
+  c[i]=b[i];
  }
  printf("\nintersection\n");
  printf("{");
  for(i=0;i<5;i++)
  {
- printf("%.2f / %s , ",c[i],name[i]);
+  printf("%.2f / %s , ",c[i],name[i]);
  }
  printf("}");
 }
@@ -59,6 +58,7 @@ void complement()
  for(i=0;i<5;i++)
  {
  printf("%.2f / %s ,",1.00-a[i],name[i]);
+ 
  }
  printf("}");
  printf("\ncomplement of B");
@@ -69,57 +69,60 @@ void complement()
  
  }
  printf("}");
- }
+ 
+}
+
+ 
 int main()
 {
  for(i=0;i<5;i++)
  {
- printf("Name=\t");
- scanf("%s",&name[i]);
- printf("Age=\t");
- scanf("%d",&age[i]);
+  printf("Name=\t");
+  scanf("%s",&name[i]);
+  printf("Age=\t");
+  scanf("%d",&age[i]);
  }
  printf("{ ");
  for(i=0;i<5;i++)
  {
- if(age[i]<=20)
- {
- a[i]=1;
- printf("%.2f / %s ",a[i],name[i]);
- }
- else if((age[i]>20)&&(age[i]<=30))
- {
- a[i]=(30-age[i])/10.00;
- printf("%.2f / %s",a[i],name[i]); 
- }
- else if(age[i]>30)
- {
- a[i]=0;
- printf("%.2f / %s",a[i],name[i]);
- }
- printf(", ");
+  if(age[i]<=20)
+  {
+   a[i]=1;
+   printf("%.2f / %s ",a[i],name[i]);
+  }
+  else if((age[i]>20)&&(age[i]<=30))
+  {
+   a[i]=(30-age[i])/10.00;
+    printf("%.2f / %s",a[i],name[i]); 
+  }
+  else if(age[i]>30)
+  {
+   a[i]=0;
+   printf("%.2f / %s",a[i],name[i]);
+  }
+  printf(", ");
  }
  printf("}");
  printf("\n");
- printf("{ ");
+  printf("{ ");
  for(i=0;i<5;i++)
  {
- if(age[i]<=15)
- {
- b[i]=1;
- printf("%.2f / %s ",b[i],name[i]);
- }
- else if((age[i]>15)&&(age[i]<=35))
- {
- b[i]=(30-age[i])/20.00;
- printf("%.2f / %s",b[i],name[i]); 
- }
- else if(age[i]>35)
- {
- b[i]=0.00;
- printf("%.f / %s",b[i],name[i]);
- }
- printf(", ");
+  if(age[i]<=15)
+  {
+   b[i]=1;
+   printf("%.2f / %s ",b[i],name[i]);
+  }
+  else if((age[i]>15)&&(age[i]<=35))
+  {
+   b[i]=(35-age[i])/20.00;
+    printf("%.2f / %s",b[i],name[i]); 
+  }
+  else if(age[i]>35)
+  {
+   b[i]=0.00;
+   printf("%.f / %s",b[i],name[i]);
+  }
+  printf(", ");
  }
  printf("}");
  printf("\n");
@@ -127,4 +130,5 @@ int main()
  intersection();
  complement();
  return 0;
+ 
 }
